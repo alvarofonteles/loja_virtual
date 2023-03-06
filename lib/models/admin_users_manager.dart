e6904e95c511baea6690afcc1373136fc55411b2
Alvaro Fonteles <alvarofonteles@hotmail.com>
2023-03-06 11:48:02 -0300

    Seção 12: Exibindo Todos os Usuários (Admin)

--- lib/models/admin_users_manager.dart
+++ lib/models/admin_users_manager.dart
@@ -1,4 +1,7 @@
-import 'package:faker/faker.dart';
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/user.dart';
 import 'package:loja_virtual/models/user_manager.dart';
@@ -6,26 +9,61 @@ import 'package:loja_virtual/models/user_manager.dart';
 class AdminUsersManager extends ChangeNotifier {
   List<User> users = [];
 
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  StreamSubscription? _subscription;
+
   void updateUser(UserManager userManager) {
-    if (userManager.adminEnabled) _listenToUsers();
+    _subscription?.cancel();
+    if (userManager.adminEnabled) {
+      _listenToUsers();
+    } else {
+      users.clear();
+      notifyListeners();
+    }
   }
 
   Future<void> _listenToUsers() async {
-    final faker = Faker();
+    // atualiza em tempo real, mas não há necessidade
+    _subscription =
+        firestore.collection('users').snapshots().listen((snapshot) {
+      users = snapshot.docs.map((d) => User.fromDocument(d)).toList();
+      // lista em ordem alfabética por nome
+      users.sort(
+          (a, b) => a.name!.toLowerCase().compareTo(b.name!.toLowerCase()));
+      notifyListeners();
+    });
 
+    /*// usuários fakes para testes
+    final faker = Faker();
     for (var i = 0; i < 200; i++) {
       users.add(User(
         name: faker.person.name(),
         email: faker.internet.email(),
       ));
-    }
+    }*/
 
+    // não atualiza em tempo real a adição de um novo usuário
+    /*firestore.collection('users').get().then((snapshot) {
+      users = snapshot.docs.map((d) => User.fromDocument(d)).toList();
+      // lista em ordem alfabética por nome
+      users.sort(
+          (a, b) => a.name!.toLowerCase().compareTo(b.name!.toLowerCase()));
+      notifyListeners();
+    });
+    
     // lista em ordem alfabética por nome
     users
         .sort((a, b) => a.name!.toLowerCase().compareTo(b.name!.toLowerCase()));
 
-    notifyListeners();
+    notifyListeners();*/
   }
 
   List<String> get names => users.map((e) => e.name!).toList();
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
 }
