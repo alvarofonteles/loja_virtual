2ea203b12dcf6e08cd9d739fcd439625b38f73f2
Alvaro Fonteles <alvarofonteles@hotmail.com>
2023-03-06 16:30:47 -0300

    65. Exibindo as Imagens no Formulário

--- lib/screens/product/product_screen.dart
+++ lib/screens/product/product_screen.dart
@@ -24,6 +24,24 @@ class ProductScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(product.name!),
           centerTitle: true,
+          actions: [
+            Consumer<UserManager>(
+              builder: (_, userManager, __) {
+                if (userManager.adminEnabled) {
+                  return IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pushReplacementNamed(
+                          '/edit_product',
+                          arguments: product);
+                    },
+                    icon: const Icon(Icons.edit),
+                  );
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ],
         ),
         backgroundColor: Colors.white,
         body: ListView(
